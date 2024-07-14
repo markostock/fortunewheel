eaa8961cbff8bd2ae453b35ec00f8f26494b9627
markostock <165729199+markostock@users.noreply.github.com>
2024-07-14 14:32:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,7 +40,9 @@ function spin() {
 //  let Eivoittoa2 = shuffle([1915,	1916,	1917,	1918,	1919,	1920,	1921,	1922,	1923,	1924,	1925,	1926,	1927,	1928,	1929,	1930,	1931,	1932,	1933,	1934,	1935,	1936,	1937,	1938,	1939,	1940,	1941,	1942,	1943,	1944]);
 //  let transkrypto200 = shuffle([1946,	1947,	1948,	1949,	1950,	1951,	1952,	1953,	1954,	1955,	1956,	1957,	1958,	1959,	1960,	1961,	1962,	1963]);
 //  let transkrypto50 = shuffle([1964,	1965,	1966,	1967,	1968,	1969,	1970,	1971,	1972,	1973,	1974,	1975,	1976,	1977,	1978,	1979]);
-    let Eivoittoa3 = shuffle([1981,	1982,	1983,	1984,	1985,	1986,	1987,	1988,	1989,	1990,	1991,	1992,	1993,	1994,	1995,	1996]);
+//  let Eivoittoa3 = shuffle([1981,	1982,	1983,	1984,	1985,	1986,	1987,	1988,	1989,	1990,	1991,	1992,	1993,	1994,	1995,	1996]);
+    let Reversed1 = shuffle([1998,	1999,	2000,	2001,	2002,	2003,	2004,	2005,	2006,	2007,	2008,	2009,	2010,	2011,	2012,	2013,	2014]);
+    let Eivoittoa4 = shuffle([2016,	2017,	2018,	2019,	2020,	2021,	2022,	2023,	2024,	2025,	2026,	2027,	2028,	2029,	2030,	2031]);
 
 
   // Random shape
@@ -51,8 +53,10 @@ function spin() {
     Näppäimistö1[0],
     Eivoittoa2[0],
     transkrypto200[0],
-    transkrypto50[0],*/
-    Eivoittoa3[0],
+    transkrypto50[0],
+    Eivoittoa3[0],*/
+    Reversed1[0],
+    Eivoittoa4[0],
   ]);
   // console.log(Hasil[0]);
 
@@ -64,8 +68,11 @@ function spin() {
   if (Näppäimistö1.includes(Hasil[0])) SelectedItem = "Näppäimistö";
   if (Eivoittoa2.includes(Hasil[0])) SelectedItem = "Ei voittoa";
   if (transkrypto200.includes(Hasil[0])) SelectedItem = "200 transkryptoa";
-  if (transkrypto50.includes(Hasil[0])) SelectedItem = "50 transkryptoa";*/
-  if (Eivoittoa3.includes(Hasil[0])) SelectedItem = "Ei voittoa";
+  if (transkrypto50.includes(Hasil[0])) SelectedItem = "50 transkryptoa";
+  if (Eivoittoa3.includes(Hasil[0])) SelectedItem = "Ei voittoa";*/
+  if (Reversed1.includes(Hasil[0])) SelectedItem = "Reversed";
+  if (Eivoittoa4.includes(Hasil[0])) SelectedItem = "Ei voittoa";
+
 
   // Process
   box.style.setProperty("transition", "all ease 5s");
@@ -77,22 +84,39 @@ function spin() {
 
   // Raise an Alert
   setTimeout(function () {
-    applause.play();
-
-    Swal.fire(
-      {
-
-      
-      imageUrl: "pikachu.gif",
-      title: SelectedItem + "!",
-      color: "white",
-      confirmButtonColor: "#ff22e2",
-      background: "transparent",
-      showConfirmButton: false,
-      backdrop: `
-      transparent
-    `
-    })
+    if (Eivoittoa4.includes(Hasil[0])) {
+      //EIVOITTOA4
+      pikachu.play();
+      Swal.fire(
+        {
+        imageUrl: "pikachu.gif",
+        title: SelectedItem + "!",
+        color: "white",
+        confirmButtonColor: "#ff22e2",
+        background: "transparent",
+        showConfirmButton: false,
+        backdrop: `
+        transparent
+        `
+      })
+    }
+    if (Reversed1.includes(Hasil[0])) {
+      //  REVERSED1
+      squirtle.play();
+      Swal.fire(
+        {
+        imageUrl: "squirtle.gif",
+        imageHeight: 250,
+        title: SelectedItem + "!",
+        color: "white",
+        confirmButtonColor: "#ff22e2",
+        background: "transparent",
+        showConfirmButton: false,
+        backdrop: `
+        transparent
+        `
+      })
+    }
 
   }, 5500);
 /*
