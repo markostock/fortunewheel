4fee5a922acbd9ff63902b6363edc4677764c83a
markostock <165729199+markostock@users.noreply.github.com>
2024-07-17 15:16:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ Eivoittoa1[0],Näppäimistö1[0],Eivoittoa2[0],transkrypto200[0],transkrypto50[0
     pikachu.play();
     Swal.fire(
       {
-      imageUrl: "eivoittoa.png",+
+      imageUrl: "eivoittoa.png",
       imageHeight: 250,
       title: SelectedItem + "!",
       color: "white",
